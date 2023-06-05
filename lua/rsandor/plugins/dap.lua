f65bbe9c79219be6c9e3787be288c854cfd3ef30
Robert Sandor <sandorrobertk94@gmail.com>
2023-06-05 11:26:31 +0300

    small changes to dap config

--- lua/rsandor/plugins/dap.lua
+++ lua/rsandor/plugins/dap.lua
@@ -3,15 +3,38 @@ return {
 		"mfussenegger/nvim-dap",
 		config = function()
 			local dap = require("dap")
+			local widgets = require("dap.ui.widgets")
+
 			vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc = "[D]ebugger toggle [B]reakpoint" })
-			vim.keymap.set("n", "<leader>dc", function() dap.continue() end, { desc = "[D]ebugger [C]ontinue" })
-			vim.keymap.set("n", "<leader>dl", function() dap.run_last() end, { desc = "[D]ebugger [L]ast" })
-			vim.keymap.set("n", "<leader>dT", function() dap.terminate() end, { desc = "[D]ebugger [T]erminate" })
-			vim.keymap.set("n", "<leader>dss", function() dap.step_over() end, { desc = "[D]ebugger [S]tep Over" })
-			vim.keymap.set("n", "<leader>dsi", function() dap.step_into() end, { desc = "[D]ebugger [S]tep [I]nto" })
-			vim.keymap.set("n", "<leader>dso", function() dap.step_out() end, { desc = "[D]ebugger [S]tep [O]ut" })
-			vim.keymap.set("n", "<leader>duh", function() widgets.hover() end, { desc = "[D]ebugger [U]I [H]over" })
-		end
+
+			vim.keymap.set("n", "<leader>dc", function()
+				dap.continue()
+			end, { desc = "[D]ebugger [C]ontinue" })
+
+			vim.keymap.set("n", "<leader>dl", function()
+				dap.run_last()
+			end, { desc = "[D]ebugger [L]ast" })
+
+			vim.keymap.set("n", "<leader>dT", function()
+				dap.terminate()
+			end, { desc = "[D]ebugger [T]erminate" })
+
+			vim.keymap.set("n", "<leader>dss", function()
+				dap.step_over()
+			end, { desc = "[D]ebugger [S]tep Over" })
+
+			vim.keymap.set("n", "<leader>dsi", function()
+				dap.step_into()
+			end, { desc = "[D]ebugger [S]tep [I]nto" })
+
+			vim.keymap.set("n", "<leader>dso", function()
+				dap.step_out()
+			end, { desc = "[D]ebugger [S]tep [O]ut" })
+
+			vim.keymap.set("n", "<leader>duh", function()
+				widgets.hover()
+			end, { desc = "[D]ebugger [U]I [H]over" })
+		end,
 	},
 	{
 		"rcarriga/nvim-dap-ui",
@@ -21,6 +44,8 @@ return {
 		opts = {},
 		config = function()
 			local dap = require("dap")
+			local dapui = require("dapui")
+
 			dap.listeners.after.event_initialized["dapui_config"] = function()
 				dapui.open()
 			end
@@ -33,15 +58,19 @@ return {
 				dapui.close()
 			end
 
-			vim.keymap.set("n", "<leader>dut", function() dapui.toggle() end, { desc = "[D]ebugger [U]I [T]oggle" })
+			vim.keymap.set("n", "<leader>dut", function()
+				dapui.toggle()
+			end, { desc = "[D]ebugger [U]I [T]oggle" })
 		end,
 	},
 	{
 		"theHamsta/nvim-dap-virtual-text",
 		dependencies = {
-			{ "mfussenegger/nvim-dap", },
-			{ "nvim-treesitter/nvim-treesitter", },
+			{ "mfussenegger/nvim-dap" },
+			{ "nvim-treesitter/nvim-treesitter" },
 		},
-		opts = {},
+		config = function()
+			require("nvim-dap-virtual-text").setup({})
+		end,
 	},
 }
