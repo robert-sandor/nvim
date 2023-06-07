426f34b5bd9a82c2873ed3951507076c5636fe2e
Robert Sandor <sandorrobertk94@gmail.com>
2023-06-07 16:07:29 +0300

    small changes to nvim-dap-go

--- lua/rsandor/plugins/go.lua
+++ lua/rsandor/plugins/go.lua
@@ -3,15 +3,17 @@ return {
 		"leoluz/nvim-dap-go",
 		ft = "go",
 		dependencies = {
-			"mfussenegger/nvim-dap"
+			"mfussenegger/nvim-dap",
 		},
-		opts = {},
+		config = function()
+			require("dap-go").setup({})
+		end,
 	},
 	{
 		"olexsmir/gopher.nvim",
 		ft = "go",
 		build = function()
-			vim.cmd [[silent! GoInstallDeps]]
+			vim.cmd([[silent! GoInstallDeps]])
 		end,
 		opts = {},
 	},
