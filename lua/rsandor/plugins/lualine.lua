a3612cb7ee6443f1c90cb264e63e0f6a0ecbf524
Robert Sandor <sandorrobertk94@gmail.com>
2023-06-08 17:48:54 +0300

    change lualine config a bit

--- lua/rsandor/plugins/lualine.lua
+++ lua/rsandor/plugins/lualine.lua
@@ -2,21 +2,34 @@ return {
 	{
 		"nvim-lualine/lualine.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			require("lualine").setup({
-				options = {
-					theme = "auto",
-					globalstatus = true,
-					section_separators = {
-						left = "",
-						right = "",
-					},
-					component_separators = {
-						left = "",
-						right = "",
-					},
+		opts = {
+			options = {
+				theme = "auto",
+				section_separators = {
+					left = "",
+					right = "",
 				},
-			})
-		end,
+				component_separators = {
+					left = "",
+					right = "",
+				},
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff" },
+				lualine_c = { "filename" },
+				lualine_x = { "encoding", "filetype" },
+				lualine_y = { "diagnostics" },
+				lualine_z = { "progress", "location" },
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { "filename" },
+				lualine_x = { "location" },
+				lualine_y = {},
+				lualine_z = {},
+			},
+		},
 	},
 }
