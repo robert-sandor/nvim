21dfafa4f0307cc0be40cc566c8dad6d884cf32b
Robert Sandor <sandorrobertk94@gmail.com>
2023-06-08 11:49:29 +0300

    adding mini.starter, and restructuring mini.lua

--- lua/rsandor/plugins/mini.lua
+++ lua/rsandor/plugins/mini.lua
@@ -1,25 +1,28 @@
 return {
-	"echasnovski/mini.nvim",
-	version = "*",
-	config = function()
-		require("mini.ai").setup()
-
-		require("mini.basics").setup({
+	{ "echasnovski/mini.ai", config = true },
+	{
+		"echasnovski/mini.basics",
+		opts = {
 			options = {
 				win_borders = "bold",
 			},
 			mappings = {
 				move_with_alt = true,
 			},
-		})
-
-		require("mini.bufremove").setup()
-		require("mini.comment").setup()
-
-		require("mini.indentscope").setup()
-		require("mini.move").setup()
-		require("mini.pairs").setup()
-		require("mini.splitjoin").setup()
-		require("mini.surround").setup()
-	end,
+		},
+	},
+	{
+		"echasnovski/mini.bufremove",
+		config = function(_, opts)
+			require("mini.bufremove").setup(opts)
+			vim.keymap.set("n", "<leader>bd", require("mini.bufremove").delete, { desc = "[B]uffer [D]elete" })
+		end,
+	},
+	{ "echasnovski/mini.comment", config = true },
+	{ "echasnovski/mini.indentscope", config = true },
+	{ "echasnovski/mini.move", config = true },
+	{ "echasnovski/mini.pairs", config = true },
+	{ "echasnovski/mini.splitjoin", config = true },
+	{ "echasnovski/mini.starter", config = true },
+	{ "echasnovski/mini.surround", config = true },
 }
