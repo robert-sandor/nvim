3f257ca3b7138e48914ff856863077657bba21ce
Robert Sandor <sandorrobertk94@gmail.com>
2023-06-12 00:02:40 +0300

    add some more integrations to catppuccin for better looks

--- lua/rsandor/plugins/colorscheme.lua
+++ lua/rsandor/plugins/colorscheme.lua
@@ -7,7 +7,23 @@ return {
 		config = function()
 			require("catppuccin").setup({
 				integrations = {
+					cmp = true,
+					dap = {
+						enabled = true,
+						enale_ui = true,
+					},
+					gitsigns = true,
+					lsp_trouble = true,
+					mason = true,
 					mini = true,
+					native_lsp = {
+						enabled = true,
+					},
+					noice = true,
+					notify = true,
+					telescope = true,
+					treesitter = true,
+					which_key = true,
 				},
 			})
 			vim.cmd.colorscheme("catppuccin")
