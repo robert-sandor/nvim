4d9825b27e4bb3009071ecac4b81a836700347c6
Robert Sandor <sandorrobertk94@gmail.com>
2023-06-11 23:38:18 +0300

    change key mappings for mini.surround

--- lua/rsandor/plugins/mini.lua
+++ lua/rsandor/plugins/mini.lua
@@ -30,5 +30,18 @@ return {
 	{ "echasnovski/mini.pairs", config = true },
 	{ "echasnovski/mini.splitjoin", config = true },
 	{ "echasnovski/mini.starter", config = true },
-	{ "echasnovski/mini.surround", config = true },
+	{
+		"echasnovski/mini.surround",
+		opts = {
+			mappings = {
+				add = "gsa", -- Add surrounding in Normal and Visual modes
+				delete = "gsd", -- Delete surrounding
+				find = "gsf", -- Find surrounding (to the right)
+				find_left = "gsF", -- Find surrounding (to the left)
+				highlight = "gsh", -- Highlight surrounding
+				replace = "gsr", -- Replace surrounding
+				update_n_lines = "gsn", -- Update `n_lines`
+			},
+		},
+	},
 }
