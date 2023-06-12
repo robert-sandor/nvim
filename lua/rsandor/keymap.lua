17c09f236dde69d30a26b5fef68e47bd7d2aa39c
Robert Sandor <sandorrobertk94@gmail.com>
2023-06-12 15:11:50 +0300

    move todo-comments keymaps to keymap.lua

--- lua/rsandor/keymap.lua
+++ lua/rsandor/keymap.lua
@@ -92,6 +92,26 @@ local keymaps = {
 		{ "<leader>ghp", "<cmd>Gitsigns preview_hunk<CR>", desc = "preview hunk" },
 		{ "<leader>ghd", "<cmd>Gitsigns diffthis<CR>",     desc = "diff this" },
 	},
+	todo_comments = {
+		{
+			"]t",
+			function()
+				require("todo-comments").jump_next()
+			end,
+			desc = "Next todo comment",
+		},
+		{
+			"[t",
+			function()
+				require("todo-comments").jump_prev()
+			end,
+			desc = "Previous todo comment",
+		},
+		{ "<leader>xt", "<cmd>TodoTrouble<cr>",                           desc = "Todo (Trouble)" },
+		{ "<leader>xT", "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>",   desc = "Todo/Fix/Fixme (Trouble)" },
+		{ "<leader>ft", "<cmd>TodoTelescope<cr>",                         desc = "[F]ind [T]odos" },
+		{ "<leader>fT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "[F]ind [T]odo/Fix/Fixme" },
+	},
 }
 
 vim.g.mapleader = " "
--- lua/rsandor/plugins/comments.lua
+++ lua/rsandor/plugins/comments.lua
@@ -1,15 +1,9 @@
 return {
 	"folke/todo-comments.nvim",
-	dependencies = { "nvim-lua/plenary.nvim"},
-    cmd = { "TodoTrouble", "TodoTelescope" },
-    event = { "BufReadPost", "BufNewFile" },
-	config = true,
-    keys = {
-      { "]t", function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
-      { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
-      { "<leader>xt", "<cmd>TodoTrouble<cr>", desc = "Todo (Trouble)" },
-      { "<leader>xT", "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
-      { "<leader>ft", "<cmd>TodoTelescope<cr>", desc = "[F]ind [T]odos" },
-      { "<leader>fT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "[F]ind [T]odo/Fix/Fixme" },
-    },
+	dependencies = { "nvim-lua/plenary.nvim" },
+	cmd = { "TodoTrouble", "TodoTelescope" },
+	event = { "BufReadPost", "BufNewFile" },
+	-- TODO something here
+	opts = {},
+	keys = require("rsandor.keymap").todo_comments,
 }
