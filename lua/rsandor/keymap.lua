87fe1cca1b8e683dbf0ce22323f0fee45cd0835c
Robert Sandor <sandorrobertk94@gmail.com>
2023-06-12 09:53:17 +0300

    moved dap keymap to the keymap file

--- lua/rsandor/keymap.lua
+++ lua/rsandor/keymap.lua
@@ -1,23 +1,54 @@
 local keymaps = {
 	base = {
 		{ "<esc>", "<cmd>nohl<CR>", desc = "remove search highlight" },
-		{ "J", "mzJ`z", desc = "Combine with line below" },
-		{ "<C-d>", "<C-d>zz", desc = "Move half page up" },
-		{ "<C-u>", "<C-u>zz", desc = "Move half page down" },
-		{ "n", "nzzzv", desc = "Next occurrence" },
-		{ "N", "Nzzzv", desc = "Previous occurrence" },
-		{ "Q", "<nop>" },
+		{ "J",     "mzJ`z",         desc = "Combine with line below" },
+		{ "<C-d>", "<C-d>zz",       desc = "Move half page up" },
+		{ "<C-u>", "<C-u>zz",       desc = "Move half page down" },
+		{ "n",     "nzzzv",         desc = "Next occurrence" },
+		{ "N",     "Nzzzv",         desc = "Previous occurrence" },
+		{ "Q",     "<nop>" },
 	},
 	telescope = {
-		{ "<leader>ff", "<cmd>Telescope find_files<CR>", desc = "find files" },
+		{ "<leader>ff", "<cmd>Telescope find_files<CR>",                            desc = "find files" },
 		{ "<leader>fa", "<cmd>Telescope find_files hidden=true no_ignore=true<CR>", desc = "find all files" },
-		{ "<leader>fw", "<cmd>Telescope live_grep<CR>", desc = "find text" },
-		{ "<leader>fo", "<cmd>Telescope buffers<CR>", desc = "find buffers" },
-		{ "<leader>fb", "<cmd>Telescope file_browser<CR>", desc = "file browser" },
-		{ "<leader>gf", "<cmd>Telescope git_files<CR>", desc = "git files" },
-		{ "<leader>gs", "<cmd>Telescope git_status<CR>", desc = "git status" },
-		{ "<leader>gb", "<cmd>Telescope git_branches<CR>", desc = "git branches" },
-		{ "<leader>gc", "<cmd>Telescope git_commits<CR>", desc = "git commits" },
+		{ "<leader>fw", "<cmd>Telescope live_grep<CR>",                             desc = "find text" },
+		{ "<leader>fo", "<cmd>Telescope buffers<CR>",                               desc = "find buffers" },
+		{ "<leader>fb", "<cmd>Telescope file_browser<CR>",                          desc = "file browser" },
+		{ "<leader>gf", "<cmd>Telescope git_files<CR>",                             desc = "git files" },
+		{ "<leader>gs", "<cmd>Telescope git_status<CR>",                            desc = "git status" },
+		{ "<leader>gb", "<cmd>Telescope git_branches<CR>",                          desc = "git branches" },
+		{ "<leader>gc", "<cmd>Telescope git_commits<CR>",                           desc = "git commits" },
+	},
+	dap = {
+		{ "<leader>db", "<cmd>DapToggleBreakpoint<CR>", desc = "toggle breakpoint" },
+		{ "<leader>dc", "<cmd>DapContinue<CR>",         desc = "continue" },
+		{ "<leader>dT", "<cmd>DapTerminate<CR>",        desc = "terminate" },
+		{
+			"<leader>dl",
+			function()
+				require("dap").run_last()
+			end,
+			desc = "run last",
+		},
+		{
+			"<leader>dh",
+			function()
+				require("dap.ui.widgets").hover()
+			end,
+			desc = "hover",
+		},
+		{ "<leader>dss", "<cmd>DapStepOver<CR>", desc = "step over" },
+		{ "<leader>dsi", "<cmd>DapStepInto<CR>", desc = "step into" },
+		{ "<leader>dso", "<cmd>DapStepOver<CR>", desc = "step over" },
+	},
+	dap_ui = {
+		{
+			"<leader>dut",
+			function()
+				require("dapui").toggle()
+			end,
+			desc = "toggle ui",
+		},
 	},
 }
 
