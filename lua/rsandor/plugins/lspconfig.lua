d67880b243bd67b16403796a579d106a59e2a91c
Robert Sandor <sandorrobertk94@gmail.com>
2023-06-15 00:38:51 +0300

    fix lsp keymap not working in nvim config dir

--- lua/rsandor/plugins/lspconfig.lua
+++ lua/rsandor/plugins/lspconfig.lua
@@ -46,30 +46,6 @@ return {
 			local mason_lspconfig = require("mason-lspconfig")
 			local capabilities = vim.lsp.protocol.make_client_capabilities()
 
-			local on_attach = function(_, buffer)
-				vim.api.nvim_buf_create_user_command(buffer, "Format", function()
-					vim.lsp.buf.format({
-						async = true,
-						bufnr = buffer,
-						filter = function(formatter)
-							return formatter.name == "null-ls"
-						end,
-					})
-				end, { desc = "Format current buffer with LSP" })
-
-				vim.api.nvim_buf_create_user_command(buffer, "FormatSync", function()
-					vim.lsp.buf.format({
-						async = false,
-						bufnr = buffer,
-						filter = function(formatter)
-							return formatter.name == "null-ls"
-						end,
-					})
-				end, { desc = "Format current buffer with LSP" })
-
-				require("rsandor.util").load_keymaps(require("rsandor.keymap").lsp, { buffer = buffer })
-			end
-
 			mason_lspconfig.setup({
 				ensure_installed = vim.tbl_keys(opts.servers),
 			})
@@ -78,9 +54,15 @@ return {
 				function(server_name)
 					require("lspconfig")[server_name].setup({
 						capabilities = capabilities,
-						on_attach = on_attach,
 						settings = opts.servers[server_name],
 					})
+				end,
+			})
+
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+				callback = function(event)
+					require("rsandor.util").load_keymaps(require("rsandor.keymap").lsp, { buffer = event.buf })
 				end,
 			})
 		end,
