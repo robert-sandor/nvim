d192accd525dba6559078284cc6854f05a3a34ef
Robert Sandor <sandorrobertk94@gmail.com>
2023-06-25 11:03:51 +0300

    remove redundant signature help

--- lua/rsandor/plugins/cmp.lua
+++ lua/rsandor/plugins/cmp.lua
@@ -5,7 +5,6 @@ return {
 		"hrsh7th/cmp-buffer",
 		"hrsh7th/cmp-path",
 		"hrsh7th/cmp-cmdline",
-		"hrsh7th/cmp-nvim-lsp-signature-help",
 		"L3MON4D3/LuaSnip",
 	},
 	event = "InsertEnter",
@@ -26,7 +25,6 @@ return {
 				end,
 			},
 			sources = cmp.config.sources({
-				{ name = "nvim_lsp_signature_help" },
 				{ name = "nvim_lsp" },
 				{ name = "luasnip" },
 				{ name = "buffer" },
