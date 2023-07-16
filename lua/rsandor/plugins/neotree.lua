23d8bcc2c4efca29614f02ebe29344dc4d17b404
Robert Sandor <sandorrobertk94@gmail.com>
2023-07-16 10:13:56 +0300

    update neotree to v3

--- lua/rsandor/plugins/neotree.lua
+++ lua/rsandor/plugins/neotree.lua
@@ -1,16 +1,13 @@
 return {
 	{
 		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v2.x",
+		branch = "v3.x",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"nvim-tree/nvim-web-devicons",
 			"MunifTanjim/nui.nvim",
 		},
 		opts = {},
-		init = function()
-			vim.g.neo_tree_remove_legacy_commands = 1
-		end,
 		keys = require("rsandor.keymap").neotree,
 	},
 }
