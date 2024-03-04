5710eb0c36e43b7e1b70fff4ba639c2cf88c6431
Robert Sandor <sandorrobertk94@gmail.com>
2024-03-04 16:11:29 +0200

    cmp-signature-help, use telescope for lsp keymaps if available

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -35,13 +35,15 @@ return {
       end
       require('mason-lspconfig').setup({ ensure_installed = ensure_installed, handlers = { lsp_setup } })
 
+      local telescope_available, telescope_builtin = pcall(require, 'telescope.builtin')
+
       vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('UserLspConfig', {}),
         callback = function(ev)
           -- Enable completion triggered by <c-x><c-o>
           vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
-          local map = function(mode, lhs, rhs, options)
+          local function map(mode, lhs, rhs, options)
             vim.tbl_extend('force', options, { buffer = ev.buf })
             vim.keymap.set(mode, lhs, rhs, options)
           end
@@ -49,17 +51,42 @@ return {
           -- Buffer local mappings.
           -- See `:help vim.lsp.*` for documentation on any of the below functions
           map('n', 'K', vim.lsp.buf.hover, { desc = 'lsp hover' })
-          map('n', 'gD', vim.lsp.buf.declaration, { desc = '[g]oto [D]eclaration' })
-          map('n', 'gd', vim.lsp.buf.definition, { desc = '[g]oto [d]efinition' })
-          map('n', 'gi', vim.lsp.buf.implementation, { desc = '[g]oto [i]mplementation' })
-          map('n', 'gr', vim.lsp.buf.references, { desc = '[g]oto [r]eferences' })
-          map('n', '<leader>cs', vim.lsp.buf.signature_help, { desc = '[c]ode [s]ignature' })
-          map('n', '<leader>ct', vim.lsp.buf.type_definition, { desc = '[c]ode [t]ype definition' })
           map('n', '<leader>cr', vim.lsp.buf.rename, { desc = '[c]ode [r]ename' })
           map({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, { desc = '[c]ode [a]ction' })
-          map('n', '<leader>cf', function()
-            vim.lsp.buf.format({ async = true })
-          end, { desc = '[c]ode [f]ormat' })
+          map('n', 'gD', vim.lsp.buf.declaration, { desc = '[g]oto [D]eclaration' })
+
+          map('n', 'gd', function()
+            if telescope_available then
+              telescope_builtin.lsp_definitions()
+            else
+              vim.lsp.buf.definition()
+            end
+          end, { desc = '[g]oto [d]efinition' })
+
+          map('n', 'gi', function()
+            if telescope_available then
+              telescope_builtin.lsp_implementations()
+            else
+              vim.lsp.buf.implementation()
+            end
+          end, { desc = '[g]oto [i]mplementation' })
+
+          map('n', 'gr', function()
+            if telescope_available then
+              telescope_builtin.lsp_references()
+            else
+              vim.lsp.buf.references()
+            end
+          end, { desc = '[g]oto [r]eferences' })
+
+          -- map('n', '<leader>cs', vim.lsp.buf.signature_help, { desc = '[c]ode [s]ignature' })
+          map('n', '<leader>ct', function()
+            if telescope_available then
+              telescope_builtin.lsp_type_definitions()
+            else
+              vim.lsp.buf.type_definition()
+            end
+          end, { desc = '[c]ode [t]ype definition' })
         end,
       })
     end,
