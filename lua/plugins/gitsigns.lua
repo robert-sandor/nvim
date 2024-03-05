342ed8c47b6cc4d5d66f2e26657eb1e7f2b275dd
Robert Sandor <sandorrobertk94@gmail.com>
2024-03-05 15:27:31 +0200

    remove gitsigns keymaps, replace mini.clue with which-key and remove dressing

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -6,26 +6,6 @@ return {
       signcolumn = true,
       numhl = true,
       current_line_blame = true,
-      on_attach = function(bufnr)
-        local gs = require('gitsigns')
-
-        local map = function(mode, l, r, desc)
-          vim.keymap.set(mode, l, r, { desc = desc, buffer = bufnr })
-        end
-
-        map('n', ']h', gs.next_hunk, 'next hunk')
-        map('n', '[h', gs.prev_hunk, 'previous hunk')
-
-        map({ 'n', 'v' }, '<leader>gs', gs.stage_hunk, '[g]it [s]tage')
-        map({ 'n', 'v' }, '<leader>gr', gs.reset_hunk, '[g]it [r]eset')
-        map('n', '<leader>gS', gs.stage_buffer, '[g]it [s]tage buffer')
-        map('n', '<leader>gR', gs.reset_buffer, '[g]it [r]eset buffer')
-        map('n', '<leader>gu', gs.undo_stage_hunk, '[g]it stage [u]ndo')
-        map('n', '<leader>gp', gs.preview_hunk, '[g]it [p]review')
-        map('n', '<leader>gb', function()
-          gs.blame_line({ full = true })
-        end, '[g]it [b]lame')
-      end,
     },
   },
 }
---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    'folke/which-key.nvim',
+    event = 'VimEnter',
+    config = function()
+      local wk = require('which-key')
+
+      wk.setup({
+        spelling = { enabled = false },
+        window = {
+          border = 'single',
+          winblend = 10,
+        },
+      })
+
+      wk.register({
+        ['<leader>b'] = { name = '[b]uffer' },
+        ['<leader>c'] = { name = '[c]ode' },
+        ['<leader>f'] = { name = '[f]ind' },
+      })
+    end,
+  },
+}
