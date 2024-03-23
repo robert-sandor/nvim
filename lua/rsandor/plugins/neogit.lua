612c47f78311ddeee7efbd6695c72a6abe712237
Robert Sandor <sandorrobertk94@gmail.com>
2024-03-23 14:55:53 +0200

    replace neogit with lazygit

--- lua/rsandor/plugins/neogit.lua
+++ lua/rsandor/plugins/neogit.lua
@@ -1,16 +1,20 @@
-return {
-  {
-    'NeogitOrg/neogit',
-    cmd = 'Neogit',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'sindrets/diffview.nvim',
-      'nvim-telescope/telescope.nvim',
-    },
-    config = true,
-    keys = {
-      { '<leader>gs', '<cmd>Neogit<cr>', desc = 'git status' },
-      { '<leader>gc', '<cmd>Neogit commit<cr>', desc = 'git commit' },
-    },
-  },
-}
+-- NOTE: Disabling neogit, as i'm mainly using lazygit anyway - to reconsider in the future
+--
+-- return {
+--   {
+--     'NeogitOrg/neogit',
+--     cmd = 'Neogit',
+--     dependencies = {
+--       'nvim-lua/plenary.nvim',
+--       'sindrets/diffview.nvim',
+--       'nvim-telescope/telescope.nvim',
+--     },
+--     config = true,
+--     keys = {
+--       { '<leader>gs', '<cmd>Neogit<cr>', desc = 'git status' },
+--       { '<leader>gc', '<cmd>Neogit commit<cr>', desc = 'git commit' },
+--     },
+--   },
+-- }
+
+return {}
