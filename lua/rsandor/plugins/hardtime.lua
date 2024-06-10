48143387d4add9ba2082728cfb205880873769d5
Robert Sandor <sandorrobertk94@gmail.com>
2024-06-10 11:20:30 +0300

    add hardtime.nvim

---
+++ lua/rsandor/plugins/hardtime.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    'm4xshen/hardtime.nvim',
+    dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
+    opts = {
+      disable_mouse = false,
+      restriction_mode = 'hint',
+    },
+  },
+}
