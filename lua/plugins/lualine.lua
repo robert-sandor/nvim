2e6227d6933b66d32a08a288e736ed4dfa83df01
Robert Sandor <sandorrobertk94@gmail.com>
2024-10-29 21:57:06 +0200

    change lualine separators

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,8 +3,8 @@ return {
     'nvim-lualine/lualine.nvim',
     opts = {
       options = {
-        section_separators = ' ',
-        component_separators = ' ',
+        section_separators = { left = '', right = ''},
+        component_separators = { left = '', right = ''},
       },
     },
   },
