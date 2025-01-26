5fd8de30fd915a49efc3880d4eb1aa1888afaa60
Robert Sandor <sandorrobertk94@gmail.com>
2025-01-26 16:11:49 +0200

    disable oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,7 +1,7 @@
 return {
   {
     'stevearc/oil.nvim',
-    enabled = true,
+    enabled = false,
     opts = {
       skip_confirm_for_simple_edits = true,
       view_options = { show_hidden = true },
