e42b269a65ce247545c25147cce590e196c0085d
Robert Sandor <sandorrobertk94@gmail.com>
2025-02-10 11:09:49 +0200

    move extras out of plugins

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,6 +18,15 @@ require('lazy').setup({
   spec = {
     -- add LazyVim and import its plugins
     { 'LazyVim/LazyVim', import = 'lazyvim.plugins' },
+    -- extras
+    { import = 'lazyvim.plugins.extras.coding.mini-surround' },
+    { import = 'lazyvim.plugins.extras.editor.dial' },
+    { import = 'lazyvim.plugins.extras.editor.illuminate' },
+    { import = 'lazyvim.plugins.extras.editor.inc-rename' },
+    { import = 'lazyvim.plugins.extras.editor.mini-files' },
+    { import = 'lazyvim.plugins.extras.util.dot' },
+    { import = 'lazyvim.plugins.extras.util.mini-hipatterns' },
+    { import = 'lazyvim.plugins.extras.util.rest' },
     -- import/override with your plugins
     { import = 'plugins' },
   },
