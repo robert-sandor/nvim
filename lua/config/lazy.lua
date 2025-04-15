8d9285bcac3063794a36ea5b46dea96b972dc1ec
Robert Sandor <sandorrobertk94@gmail.com>
2025-04-15 12:26:54 +0300

    switch back to oil.nvim

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -24,7 +24,7 @@ require('lazy').setup({
     { import = 'lazyvim.plugins.extras.editor.dial' },
     { import = 'lazyvim.plugins.extras.editor.illuminate' },
     { import = 'lazyvim.plugins.extras.editor.inc-rename' },
-    { import = 'lazyvim.plugins.extras.editor.mini-files' },
+    -- { import = 'lazyvim.plugins.extras.editor.mini-files' },
     { import = 'lazyvim.plugins.extras.editor.mini-move' },
     { import = 'lazyvim.plugins.extras.util.dot' },
     { import = 'lazyvim.plugins.extras.util.mini-hipatterns' },
--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,8 +1,9 @@
 return {
   {
     'stevearc/oil.nvim',
-    enabled = false,
+    enabled = true,
     opts = {
+      default_file_explorer = true,
       skip_confirm_for_simple_edits = true,
       view_options = { show_hidden = true },
       float = { padding = 4 },
