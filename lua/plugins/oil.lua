5c4b19e24c66b2a82502ce926b5ae81ffba7154b
Robert Sandor <sandorrobertk94@gmail.com>
2025-05-09 10:16:15 +0300

    oil: simple keymap to close

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -7,6 +7,7 @@ return {
       skip_confirm_for_simple_edits = true,
       view_options = { show_hidden = true },
       float = { padding = 4 },
+      keymaps = { ['q'] = { 'actions.close', mode = 'n' } },
     },
     dependencies = { { 'echasnovski/mini.icons', opts = {} } },
     keys = {
