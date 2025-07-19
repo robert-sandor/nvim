e9aecb187927d9de42c8b483603946a9d63bc988
Robert Sandor <sandorrobertk94@gmail.com>
2025-07-20 00:42:26 +0300

    todo-comments - updates to keywords

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -7,18 +7,21 @@ return function()
   require('todo-comments').setup({
     signs = false,
     keywords = {
-      FIX = { alt = { 'fix', 'fixme', 'FIXME' } },
+      FIX = { alt = { 'FIXME', 'BUG', 'FIXIT', 'ISSUE', 'fix', 'fixme', 'bug', 'fixit', 'issue' } },
       TODO = { alt = { 'todo' } },
       HACK = { alt = { 'hack' } },
-      WARN = { alt = { 'warn', 'warning', 'WARNING' } },
-      NOTE = { alt = { 'note', 'info', 'INFO' } },
+      WARN = { alt = { 'WARNING', 'XXX', 'warn', 'warning', 'xxx' } },
+      NOTE = { alt = { 'INFO', 'note', 'info' } },
     },
     highlight = {
+      before = 'fg',
       keyword = 'fg',
       after = 'fg',
     },
   })
 
   local utils = require('utils')
-  utils.key('n', '<leader>ft', utils.cmd('TodoQuickFix'), 'find todos')
+  utils.key('n', '<leader>ft', utils.cmd('TodoFzfLua'), 'find todos')
+  utils.key('n', ']t', require('todo-comments').jump_next, 'Next todo comment')
+  utils.key('n', '[t', require('todo-comments').jump_prev, 'Previous todo comment')
 end
