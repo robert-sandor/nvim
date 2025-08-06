ab13476e30958ccc01e5d9e8ccb144a8b5d8e9ee
Robert Sandor <sandorrobertk94@gmail.com>
2025-08-06 13:39:45 +0300

    remove redundant keymapings

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -6,11 +6,6 @@ key('n', '<esc>', cmd('nohl'), 'Clear search highlights')
 vim.keymap.set('x', '<leader>p', '"_dP', { desc = 'Paste without yanking' })
 vim.keymap.set({ 'n', 'v' }, '<leader>d', '"_d', { desc = 'Delete without yanking' })
 
-vim.keymap.set('n', '<A-j>', ':m .+1<CR>==', { desc = 'Move line down' })
-vim.keymap.set('n', '<A-k>', ':m .-2<CR>==', { desc = 'Move line up' })
-vim.keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv", { desc = 'Move selection down' })
-vim.keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv", { desc = 'Move selection up' })
-
 vim.keymap.set('v', '<', '<gv', { desc = 'Indent left and reselect' })
 vim.keymap.set('v', '>', '>gv', { desc = 'Indent right and reselect' })
 
