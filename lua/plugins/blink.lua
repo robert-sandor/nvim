46a7b61485e0b36b003affc7a1f2a034b90e9021
Robert Sandor <sandorrobertk94@gmail.com>
2025-09-13 13:02:49 +0300

    update blink and use default keymap

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -2,10 +2,8 @@ return function()
   require('mini.deps').add({
     source = 'saghen/blink.cmp',
     depends = { 'rafamadriz/friendly-snippets' },
-    checkout = 'v1.4.1',
+    checkout = 'v1.6.0',
   })
 
-  require('blink.cmp').setup({
-    keymap = { preset = 'enter' },
-  })
+  require('blink.cmp').setup({})
 end
