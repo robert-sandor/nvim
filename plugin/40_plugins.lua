884beba2e207d19703c8dedf818c9914fdb170eb
Robert Sandor <sandorrobertk94@gmail.com>
2025-11-19 14:46:31 +0200

    lsp - add mason and friends for automatic configuration, and nvim-lint for linting

--- plugin/40_plugins.lua
+++ plugin/40_plugins.lua
@@ -1,6 +1,7 @@
 local minideps = require('mini.deps')
 local add, now, later = minideps.add, minideps.now, minideps.later
 local now_if_args = _G.Config.now_if_args
+local new_autocmd = _G.Config.new_autocmd
 
 -- theme
 now(function()
@@ -59,10 +60,24 @@ end)
 -- lsp
 now_if_args(function()
   add('neovim/nvim-lspconfig')
-  vim.lsp.enable({
-    'ansiblels',
-    'docker_compose_language_service',
-    'lua_ls',
+  add('mason-org/mason.nvim')
+  add('mason-org/mason-lspconfig.nvim')
+  add('WhoIsSethDaniel/mason-tool-installer.nvim')
+
+  require('mason').setup()
+  require('mason-lspconfig').setup()
+  require('mason-tool-installer').setup({
+    ensure_installed = {
+      'lua_ls', -- Lua language server
+      'stylua', -- Lua formatter
+      'ansiblels', -- Ansible language server
+      'docker_language_server', -- Docker language server
+      'shellcheck', -- Bash linter
+      'shfmt', -- Bash formatter
+      'pyright', -- Python LSP
+      'ruff', -- Python linter and formatter
+      'prettierd', -- json, yaml, etc. formatter
+    },
   })
 end)
 
@@ -76,8 +91,22 @@ later(function()
       lua = { 'stylua' },
       sh = { 'shfmt' },
       yaml = { 'prettierd' },
+      python = { 'ruff_format', 'ruff_organize_imports' },
     },
   })
+end)
+
+later(function()
+  add('mfussenegger/nvim-lint')
+
+  require('lint').linters_by_ft = {
+    python = { 'ruff' },
+    sh = { 'shellcheck' },
+  }
+
+  new_autocmd({ 'BufReadPost', 'BufWritePost' }, '*', function()
+    require('lint').try_lint()
+  end, 'Try linting file')
 end)
 
 -- snippets
