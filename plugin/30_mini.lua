8f2c7023300fead5e7f707d26698b9b93d01772c
Robert Sandor <sandorrobertk94@gmail.com>
2026-01-27 23:42:56 +0200

    mini.files - add bindings for opening files in splits

--- plugin/30_mini.lua
+++ plugin/30_mini.lua
@@ -35,7 +35,6 @@ later(function()
   require('mini.bracketed').setup()
   require('mini.diff').setup()
   require('mini.extra').setup()
-  require('mini.files').setup({ windows = { preview = true } })
   require('mini.git').setup()
   require('mini.indentscope').setup()
   require('mini.jump').setup()
@@ -45,6 +44,38 @@ later(function()
   require('mini.splitjoin').setup()
   require('mini.surround').setup()
   require('mini.trailspace').setup()
+end)
+
+later(function()
+  local minifiles = require('mini.files')
+  minifiles.setup({ windows = { preview = true } })
+
+  local map_split = function(buf_id, lhs, direction)
+    local rhs = function()
+      -- Make new window and set it as target
+      local cur_target = minifiles.get_explorer_state().target_window
+      local new_target = vim.api.nvim_win_call(cur_target, function()
+        vim.cmd(direction .. ' split')
+        return vim.api.nvim_get_current_win()
+      end)
+
+      minifiles.set_target_window(new_target)
+    end
+
+    local desc = 'Split ' .. direction
+    vim.keymap.set('n', lhs, rhs, { buffer = buf_id, desc = desc })
+  end
+
+  vim.api.nvim_create_autocmd('User', {
+    pattern = 'MiniFilesBufferCreate',
+    callback = function(args)
+      local buf_id = args.data.buf_id
+      -- Tweak keys to your liking
+      map_split(buf_id, '<C-s>', 'belowright horizontal')
+      map_split(buf_id, '<C-v>', 'belowright vertical')
+      map_split(buf_id, '<C-t>', 'tab')
+    end,
+  })
 end)
 
 later(function()
