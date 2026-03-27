4ef9d154e524ed097d15a3359b1f566d55f8445e
Robert Sandor <sandorrobertk94@gmail.com>
2026-03-27 12:17:33 +0200

    remove a bunch of options that are unnecessary

--- plugin/10_options.lua
+++ plugin/10_options.lua
@@ -3,22 +3,15 @@ vim.g.mapleader = ' ' -- Use `<Space>` as <Leader> key
 
 vim.o.mouse = 'a' -- Enable mouse
 vim.o.mousescroll = 'ver:1,hor:6' -- Customize mouse scroll
-vim.o.switchbuf = 'usetab' -- Use already opened buffers when switching
 vim.o.undofile = true -- Enable persistent undo
 vim.o.confirm = true -- Enable confimation for abandoning buffers, and others
 
-vim.o.shada = "'100,<50,s10,:1000,/100,@100,h" -- Limit ShaDa file (for startup)
-
 -- Enable all filetype plugins and syntax (if not enabled, for better startup)
 vim.cmd('filetype plugin indent on')
-if vim.fn.exists('syntax_on') ~= 1 then
-  vim.cmd('syntax enable')
-end
+vim.cmd('syntax enable')
 
 -- UI =========================================================================
 vim.o.breakindent = true -- Indent wrapped lines to match line start
-vim.o.breakindentopt = 'list:-1' -- Add padding for lists (if 'wrap' is set)
-vim.o.colorcolumn = '+1' -- Draw column on the right of maximum width
 vim.o.cursorline = true -- Enable current line highlighting
 vim.o.cursorlineopt = 'screenline,number' -- Show cursor line per screen line
 vim.o.linebreak = true -- Wrap lines at 'breakat' (if 'wrap' is set)
@@ -26,19 +19,12 @@ vim.o.list = true -- Show helpful text indicators
 vim.o.number = true -- Show line numbers
 vim.o.pumheight = 10 -- Make popup menu smaller
 vim.o.relativenumber = true -- Show relative line numbers
-vim.o.ruler = false -- Don't show cursor coordinates
-vim.o.shortmess = 'CFOSWaco' -- Disable some built-in completion messages
 vim.o.showmode = false -- Don't show mode in command line
 vim.o.signcolumn = 'yes' -- Always show signcolumn (less flicker)
 vim.o.splitbelow = true -- Horizontal splits will be below
-vim.o.splitkeep = 'screen' -- Reduce scroll during window split
 vim.o.splitright = true -- Vertical splits will be to the right
 vim.o.winborder = 'single' -- Use border in floating windows
 vim.o.wrap = false -- Don't visually wrap lines (toggle with \w)
-
--- Special UI symbols. More is set via 'mini.basics' later.
-vim.o.fillchars = 'eob: ,fold:╌'
-vim.o.listchars = 'extends:…,nbsp:␣,precedes:…,tab:> '
 
 -- Folds (see `:h fold-commands`, `:h zM`, `:h zR`, `:h zA`, `:h zj`)
 vim.o.foldlevel = 10 -- Fold nothing by default; set to 0 or 1 to fold
@@ -49,7 +35,7 @@ vim.o.foldtext = '' -- Show text under fold with its highlighting
 -- Editing ====================================================================
 vim.o.autoindent = true -- Use auto indent
 vim.o.expandtab = true -- Convert tabs to spaces
-vim.o.formatoptions = 'rqnl1j' -- Improve comment editing
+vim.o.formatoptions = 'rqnl1j' -- Improve comment editing - default tcqj
 vim.o.ignorecase = true -- Ignore case during search
 vim.o.incsearch = true -- Show search matches while typing
 vim.o.infercase = true -- Infer case in built-in completion
@@ -64,14 +50,9 @@ vim.o.spelloptions = 'camel' -- Treat camelCase word parts as separate words
 vim.o.tabstop = 2 -- Show tab as this number of spaces
 vim.o.virtualedit = 'block' -- Allow going past end of line in blockwise mode
 
--- Pattern for a start of numbered list (used in `gw`). This reads as
--- "Start of list item is: at least one special character (digit, -, +, *)
--- possibly followed by punctuation (. or `)`) followed by at least one space".
-vim.o.formatlistpat = [[^\s*[0-9\-\+\*]\+[\.\)]*\s\+]]
-
 -- Built-in completion
 vim.o.complete = '.,w,b,kspell' -- Use less sources
-vim.o.completeopt = 'menuone,noselect,fuzzy,nosort' -- Use custom behavior
+vim.o.completeopt = 'menuone,noselect,fuzzy,nosort,popup' -- Use custom behavior
 
 -- Autocommands ===============================================================
 
