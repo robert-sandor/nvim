aa693b581c74e52ac8d62f815699bc1d422ac78e
Robert Sandor <sandorrobertk94@gmail.com>
2026-04-05 14:20:21 +0300

    conform - add formatting for erlang through rebar3 fmt

--- lua/plugins/customizations.lua
+++ lua/plugins/customizations.lua
@@ -3,4 +3,17 @@ return {
 
   -- disable plugins
   { "akinsho/bufferline.nvim", enabled = false },
+
+  -- formatting config
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters = {
+        rebar3 = { command = "rebar3", args = { "fmt", "-" } },
+      },
+      formatters_by_ft = {
+        erlang = { "rebar3" },
+      },
+    },
+  },
 }
