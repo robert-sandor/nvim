d0360fd02576124e2f68a2e4a44d2c952881ee0c
Robert Sandor <sandorrobertk94@gmail.com>
2026-08-28 15:09:34 +0300

    update plugins

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -47,7 +47,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('custom-lsp-detach', { clear = true }),
         callback = function(event2)
           vim.lsp.buf.clear_references()
-          vim.api.nvim_clear_autocmds({ group = 'kickstart-lsp-highlight', buffer = event2.buf })
+          vim.api.nvim_clear_autocmds({ group = 'custom-lsp-highlight', buffer = event2.buf })
         end,
       })
     end
