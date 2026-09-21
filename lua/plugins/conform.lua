844cde969f9c6073812338eae4a8af069424bd73
Robert Sandor <sandorrobertk94@gmail.com>
2026-09-21 10:14:46 +0300

    add shfmt as formatter for sh, bash and zsh

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,6 +2,9 @@ require('conform').setup({
   format_on_save = { timeout_ms = 500 },
   default_format_opts = { lsp_format = 'fallback' },
   formatters_by_ft = {
+    bash = { 'shfmt' },
+    zsh = { 'shfmt' },
+    sh = { 'shfmt' },
     python = { 'isort', 'black' },
   },
 })
